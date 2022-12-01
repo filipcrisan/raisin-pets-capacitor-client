1756281946e24b27b1f6680858ef7e12b6d84e1b
Filip Crisan <66752246+filipcrisan@users.noreply.github.com>
2022-12-01 13:33:04 +0200

    feat: add android and ios apps with initial angular project

---
+++ android/app/src/main/java/com/raisinpets/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.raisinpets.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.raisinpets.app',
+  appName: 'raisin-pets-capacitor-client',
+  webDir: 'dist/raisin-pets-capacitor-client',
+  bundledWebRuntime: false
+};
+
+export default config;
