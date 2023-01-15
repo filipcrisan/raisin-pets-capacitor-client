2c769aee47fc101c3612239d838445fea56ad590
Filip Crisan <66752246+filipcrisan@users.noreply.github.com>
2023-01-15 21:51:39 +0200

    feat(app): add tailwind and some landing page components

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "landing-page-yellow": "#eee4e5",
+      },
+    },
+  },
+  plugins: [],
+};
