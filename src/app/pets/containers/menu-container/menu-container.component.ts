e55f0b37fe24adf537c06f8e6704d30633eba1de
Filip Crisan <66752246+filipcrisan@users.noreply.github.com>
2023-02-12 14:13:18 +0200

    feat: add logout

--- src/app/pets/containers/menu-container/menu-container.component.ts
+++ src/app/pets/containers/menu-container/menu-container.component.ts
@@ -1,11 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AuthFacades } from '../../../facades/auth.facades';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-menu-container',
   templateUrl: './menu-container.component.html',
   styleUrls: ['./menu-container.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MenuContainerComponent {
+  authQuery = this.authFacades.query;
 
+  constructor(private authFacades: AuthFacades, private location: Location) {}
+
+  onClose(): void {
+    this.location.back();
+  }
+
+  onLogout(): void {
+    this.authFacades.logout();
+  }
 }
