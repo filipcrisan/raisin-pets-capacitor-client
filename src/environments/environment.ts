29ea9856451ce7e2ecf71f5f46a13fb09c7d427e
Filip Crisan <66752246+filipcrisan@users.noreply.github.com>
2023-03-19 16:48:39 +0200

    refactor: make button container global style and others

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://efaa-188-27-131-120.eu.ngrok.io/api',
+  apiUrl: 'https://9374-188-27-131-120.eu.ngrok.io/api',
   angularGoogleClientId:
     '934410184091-c2c7tavjvscau815l48a7uemu1c26fam.apps.googleusercontent.com',
 };
