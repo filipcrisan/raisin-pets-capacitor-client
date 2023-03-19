1c4d8a9350102358117f265d7d5ca7cfc705ac32
Filip Crisan <66752246+filipcrisan@users.noreply.github.com>
2023-03-19 11:10:04 +0200

    refactor: use on changes

--- src/app/pets/components/exercise-details/exercise-details.component.ts
+++ src/app/pets/components/exercise-details/exercise-details.component.ts
@@ -1,15 +1,15 @@
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
+  OnChanges,
   Output,
   ViewChild,
 } from '@angular/core';
 import { Exercise } from '../../models/exercise.model';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgChanges } from '../../../shared/models/simple-changes-typed';
 import { GoogleMap } from '@angular/google-maps';
 
 @Component({
@@ -18,50 +18,53 @@ import { GoogleMap } from '@angular/google-maps';
   styleUrls: ['./exercise-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExerciseDetailsComponent implements AfterViewInit {
+export class ExerciseDetailsComponent implements OnChanges {
   @Input() exercise: Exercise;
   @Input() loading: boolean;
   @Input() error: HttpErrorResponse;
 
   @Output() back = new EventEmitter<void>();
 
-  center: google.maps.LatLngLiteral;
   vertices: google.maps.LatLngLiteral[] = [];
   options: google.maps.MapOptions = {
     fullscreenControl: false,
+    streetViewControl: false,
+    zoomControl: false,
+    mapTypeControl: false,
   };
 
   @ViewChild('googleMap') googleMap: GoogleMap;
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  ngOnChanges(changes: NgChanges<ExerciseDetailsComponent>): void {
+    if (changes.exercise?.currentValue) {
+      this.vertices = this.exercise.checkpoints?.map((x) => ({
+        lat: x.latitude,
+        lng: x.longitude,
+      }));
 
-  ngAfterViewInit(): void {
-    this.vertices = this.exercise.checkpoints?.map((x) => ({
-      lat: x.latitude,
-      lng: x.longitude,
-    }));
-
-    this.processVertices();
+      this.onVerticesChanges();
+    }
   }
 
   onBack(): void {
     this.back.emit();
   }
 
-  private processVertices(): void {
+  private onVerticesChanges(): void {
     const bounds = new google.maps.LatLngBounds();
 
     this.vertices.forEach((marker: any) => {
       bounds.extend(new google.maps.LatLng(marker.lat, marker.lng));
     });
 
-    this.googleMap.fitBounds(bounds);
+    this.googleMap?.fitBounds(bounds);
 
-    this.center = {
-      lat: bounds.getCenter().lat(),
-      lng: bounds.getCenter().lng(),
+    this.options = {
+      ...this.options,
+      center: {
+        lat: bounds.getCenter().lat(),
+        lng: bounds.getCenter().lng(),
+      },
     };
-
-    this.cdr.detectChanges();
   }
 }
