cde79db292c33a0d75f3d3064d6e7194203dc26c
Filip Crisan <66752246+filipcrisan@users.noreply.github.com>
2023-03-26 11:24:09 +0300

    refactor: add new cloud api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://d34f-188-27-128-39.eu.ngrok.io/api',
+  apiUrl: 'https://raisin-pets.azurewebsites.net/api',
   angularGoogleClientId:
     '934410184091-c2c7tavjvscau815l48a7uemu1c26fam.apps.googleusercontent.com',
 };
